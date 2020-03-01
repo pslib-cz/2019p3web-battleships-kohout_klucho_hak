6932d8423e78316f4ae45ac9671739a2fe626ced
Vojtěch Klucho <Vojtech.Klucho@365.pslib.cz>
2020-03-01 15:27:46 +0100

    Vytvoření Pages - Přidána stránka pro vytvoření hry - Přidána stránka pro rozmisťování lodí - Přidána stránka všech her (pravděpodobně bude nějaké lepší pojmenování) - Do databáze byl přidán navíc user - Drobné úpravy Navbaru (Pages/Shared/Layout a LoginPartial)

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -8,10 +8,17 @@ namespace BattleShips.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+
         }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //}
+
+        public DbSet<User> User { get; set; }
         public DbSet<Game> Game { get; set; }
         public DbSet<NavyBattlePiece> NavyBattlePiece { get; set; }
         public DbSet<Ship> Ship { get; set; }
