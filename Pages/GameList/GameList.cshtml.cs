4c9d156b4dee531e0c454b202b2c5955515cd980
Vojtěch Klucho <Vojtech.Klucho@365.pslib.cz>
2020-03-23 09:55:06 +0100

    GameList

--- Pages/GameList/GameList.cshtml.cs
+++ Pages/GameList/GameList.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BattleShips.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,13 @@ namespace BattleShips
 {
     public class GameListModel : PageModel
     {
+        public GameListModel()
+        {
+            GameLists = new List<GameListViewModel>();
+        }
+
+        public List<GameListViewModel> GameLists { get; set; }
+
         public void OnGet()
         {
 
---
+++ ViewModels/GameListViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleShips.ViewModels
+{
+    public class GameListViewModel
+    {
+
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+
+        [Display(Name = "GameCreatedAt")]
+        public string GameCreatedAt { get; set; }
+
+
+        [Display(Name = "GameState")]
+        public string GameState { get; set; }
+
+
+        [Display(Name = "Player1")]
+        public string Player1 { get; set; }
+
+
+        [Display(Name = "Player2")]
+        public string Player2 { get; set; }
+
+        [Display(Name = "Remove")]
+        public string Remove { get; set; }
+    }
+}
