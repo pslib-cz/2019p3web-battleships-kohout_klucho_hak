9bffde566116eacd1e6fa4f4126b8b9135d18947
Vojtěch Klucho <vojtech.klucho@pslib.cz>
2020-04-19 12:47:28 +0200

    GameSetup.cs

--- Pages/CreateGame/GameSetup.cshtml.cs
+++ Pages/CreateGame/GameSetup.cshtml.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BattleShips.Data;
+using BattleShips.Services;
+using BattleShips.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +12,23 @@ namespace BattleShips
 {
     public class GameSetupModel : PageModel
     {
+        public ApplicationDbContext _db;
+
+        public IGameSetup _igamesetup;
+
+        public ShipViewModel Ship { get; set; }
+        public int boardSize { get; set; }
+        public List<Ship> Ships { get; set; }
+
+
+        public GameSetupModel(ApplicationDbContext db, IGameSetup igamesetup)
+        {
+            _db = db;
+            _igamesetup = igamesetup;
+        }
+
+      
+
         public void OnGet()
         {
 
