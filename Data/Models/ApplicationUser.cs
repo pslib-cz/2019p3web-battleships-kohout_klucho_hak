ae4d34b48bcc2843b0fa028c9a74763e0d442250
Robert <48084300+Robbiexd@users.noreply.github.com>
2020-05-11 15:30:19 +0200

    small clean up

--- Data/Models/ApplicationUser.cs
+++ Data/Models/ApplicationUser.cs
@@ -11,36 +11,12 @@ namespace BattleShips.Data
 {
     public class ApplicationUser : IdentityUser
     {
-        [Required]
-        [MaxLength(20, ErrorMessage = "You need to keep the name to max of 20 characters.")]
-        [MinLength(3, ErrorMessage = "You need to enter at least 3 characters for an Username")]
-        [DisplayName("Username")]
+        
         public string PlayerName { get; set; }
 
         public int Wins { get; set; }
 
         public int TotalPlayedGames { get; set; }
 
-
-        ////public double WinRate => ((Wins / TotalPlayedGames) * 100);
-        //[NotMapped]
-        //private double _winRate;
-
-        //[NotMapped]
-        //public double WinRate
-        //{
-        //    get { return _winRate; }
-        //    set 
-        //    {
-        //        if(TotalPlayedGames !=0 || Wins !=0)
-        //        {
-        //            _winRate = ((Wins / TotalPlayedGames) * 100);
-        //        }
-        //        else
-        //        {
-        //            _winRate = 0;
-        //        }
-        //    }
-        //}
     }
 }
