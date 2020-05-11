7568e037a738cc1c7c2f1e904422b7421c2cbff1
Robert <48084300+Robbiexd@users.noreply.github.com>
2020-05-11 14:58:17 +0200

    Added PlayerName to be displayed in Navbar

---
+++ ViewComponents/PlayerNameViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using BattleShips.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BattleShips.ViewComponents
+{
+    public class PlayerNameViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PlayerNameViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var userFromDb = await _db.ApplicationUsers.FirstOrDefaultAsync(a => a.Id == claims.Value);
+            return View(userFromDb);
+        }
+    }
+}
